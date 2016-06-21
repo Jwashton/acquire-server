14982f93dc2c46c09109fc8704423aa875d950b7
William Ashton <William@AshtonFam.org>
2016-06-20 22:40:30 -0400

    Only perform label calculation once

--- src/tile.js
+++ src/tile.js
@@ -4,24 +4,22 @@ const FIRST_COL = 0;
 const CHAR_CODE_OFFSET = 65;
 const LABEL_OFFSET = 1;
 
+const calculateLabel = function calculateLabel(row, col) {
+  const rowLabel = String.fromCharCode(CHAR_CODE_OFFSET + row);
+  const colLabel = col + LABEL_OFFSET;
+
+  return colLabel + rowLabel;
+};
+
 // Tile factory
 //
 // Takes 0-indexed row and col values
 const createTile = function createTile(row = FIRST_ROW, col = FIRST_COL) {
-  const tile = { row, col };
-
-  const methods = {
-
-    // Calculate a human-readable label for this tile
-    get label() {
-      const rowLabel = String.fromCharCode(CHAR_CODE_OFFSET + row);
-      const colLabel = col + LABEL_OFFSET;
-
-      return colLabel + rowLabel;
-    }
+  return {
+    row,
+    col,
+    label: calculateLabel(row, col)
   };
-
-  return Object.assign(tile, methods);
 };
 
 export default createTile;
