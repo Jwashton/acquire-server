e53dccaa2c7d140c9bfe0ec8091b9dd8aabab051
William Ashton <William@AshtonFam.org>
2016-06-20 22:27:39 -0400

    Refactor tests

--- test/tileTest.js
+++ test/tileTest.js
@@ -1,32 +1,30 @@
 import test from 'ava';
 import createTile from '../src/tile.js';
 
+test.beforeEach(t => {
+  t.context.a1 = createTile(0, 0);
+  t.context.c4 = createTile(2, 3);
+  t.context.d3 = createTile(3, 2);
+});
+
 test('row and col default to 0', t => {
   const blank = createTile();
-  const a1    = createTile(0, 0);
 
-  t.deepEqual(blank, a1);
+  t.deepEqual(blank, t.context.a1);
 });
 
 test('A newly constructed tile has a row', t => {
-  const c1 = createTile(3, 0);
-
-  t.is(c1.row, 3);
+  t.is(t.context.c4.row, 2);
 });
 
 test('A newly constructed tile has a col', t => {
-  const c4 = createTile(3, 4);
-
-  t.is(c4.col, 4);
+  t.is(t.context.c4.col, 3);
 });
 
 test.todo('A newly constructed tile is empty');
 test.todo('A newly constructed tile is not a member of a chain');
 
 test('A tile has a label', t => {
-  const c4 = createTile(2, 4);
-  const d3 = createTile(3, 3);
-
-  t.is(c4.label, '5C');
-  t.is(d3.label, '4D');
+  t.is(t.context.c4.label, '4C');
+  t.is(t.context.d3.label, '3D');
 });
