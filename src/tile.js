9bb45b446cd70be6efed9657c71c215c224542bd
William Ashton <William@AshtonFam.org>
2016-06-20 22:37:10 -0400

    Add some documentation

--- src/tile.js
+++ src/tile.js
@@ -4,10 +4,15 @@ const FIRST_COL = 0;
 const CHAR_CODE_OFFSET = 65;
 const LABEL_OFFSET = 1;
 
+// Tile factory
+//
+// Takes 0-indexed row and col values
 const createTile = function createTile(row = FIRST_ROW, col = FIRST_COL) {
   const tile = { row, col };
 
   const methods = {
+
+    // Calculate a human-readable label for this tile
     get label() {
       const rowLabel = String.fromCharCode(CHAR_CODE_OFFSET + row);
       const colLabel = col + LABEL_OFFSET;
